76c85983c152c12802e6d9d6abcc5088da0d4ad5
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-14 21:27:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function moveDodgerRight() {
  var newPosition = dodger.style.left.replace("px", "");
     newPosition = parseInt(newPosition, 10);
  
-  if (newPosition > 0 && newPosition < 361) {
+  if (newPosition >= 0 && newPosition < 361) {
     dodger.style.left = `${newPosition + 1}px`;
   }
 }
